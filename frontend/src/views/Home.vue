6095f73e7f58813f3123e5f86e84fb0bee7861ed
naoki <naokipgt@gmail.com>
2021-10-03 17:04:03 +0900

    issue-13 タイトルとメニューを更新

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-  top page
+    <h2>
+      Digital E-Paper Photo Frame
+    </h2>
+    <p>
+      電子ペーパーに好きな写真を映すことのできるアプリです
+    </p>
+    <p>
+      写真を表示したい場合は，左のメニューから  IMAGE を選択
+    </p>
+    <p>
+      映っている内容を削除したい場合は，左のメニューから CLEAR を選択
+    </p>
+
+    <p>
+      Safari 下のメニューから， 「HOME 画面に追加」 をしておくと，すぐにアクセスできます
+    </p>
   </div>
 </template>
 
