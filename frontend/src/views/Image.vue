0c5b7e0c349939a2da23b0451a7c031d48a744ae
naoki <naokipgt@gmail.com>
2021-10-04 23:40:06 +0900

    issue-16 pager と 画像クリックでダイアログを生成する機能実装

--- frontend/src/views/Image.vue
+++ frontend/src/views/Image.vue
@@ -2,23 +2,53 @@
   <div>
     <LoadingDialog v-bind:enable="isLoading" v-bind:text="loadingText"/>
 
-    <div>
+    <v-dialog
+      v-model="dialog"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
+      <v-card>
+        <v-toolbar
+          dark
+          color="primary"
+        >
+          <v-btn
+          outlined
+          elevation="5"
+            dark
+            @click="dialog = false"
+          >
+          CANCEL
+          </v-btn>
+        </v-toolbar>
+      </v-card>
+    </v-dialog>
+
+    <div class="album">
       <v-row>
         <v-col
-          v-for="picture in pictureNameList"
+          v-for="picture in currentPagePictureList"
           :key="picture"
           class="d-flex child-flex"
-          cols="6"
+          cols="4"
         >
-          <v-img
-            :src="`/api/image/${picture}`"
-            aspect-ratio="1"
-            class="grey lighten-2"
+          <v-card
+            @click.stop="cardClicked(picture)"
           >
-          {{ picture }}
-          </v-img>
+            <v-img
+              :src="`/api/image/${picture}`"
+              aspect-ratio="1.333"
+              class="grey lighten-2"
+            >
+            </v-img>
+          </v-card>
         </v-col>
       </v-row>
+      <v-pagination
+        v-model="page"
+        :length="pageMax"
+      ></v-pagination>
       <!-- {{ pictureNameList }} -->
     </div>
   </div>
@@ -40,6 +70,28 @@ export default {
       isLoading: false,
       loadingText: '初期化中...',
       pictureNameList: [],
+      imageCol: 2,
+      imageRow: 3,
+      page: 1,
+      dialog: false,
+      selectPictureName: '',
+    }
+  },
+
+  computed: {
+    singlePageNum() {
+      return this.imageCol * this.imageRow
+    },
+
+    pageMax() {
+      return Math.ceil(this.pictureNameList.length/(this.singlePageNum))
+    },
+
+    currentPagePictureList() {
+      return this.pictureNameList.slice(
+        (this.page-1)*this.singlePageNum,
+        this.page * this.singlePageNum
+      )
     }
   },
 
@@ -51,6 +103,16 @@ export default {
   },
 
   methods: {
+    cardClicked(name) {
+      this.selectPictureName = name
+      this.dialog = true
+    },
+    getImage(name) {
+      axios.get(`/api/image/${name}`, {}).then((response) => {
+        console.log(response)
+        return response.data
+      })
+    },
     // clear() {
     //   this.isLoading = true
     //   axios.post('/api/clear', {
@@ -64,3 +126,10 @@ export default {
   }
 }
 </script>
+
+<style>
+
+.album {
+  padding: 16px;
+}
+</style>
