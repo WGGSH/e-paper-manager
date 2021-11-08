63f71f7017faf61b6fbec854ec6dc424fd42565b
naoki <naokipgt@gmail.com>
2021-11-09 00:19:17 +0900

    issue-37 設定ページ作成

---
+++ frontend/src/views/Settings.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="settings">
+    <div v-for="param in params" :key="param.label">
+      <v-switch
+        v-model="param.value"
+        :label="param.name"
+        color="primary"
+        @change="onChangeValue"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      params: [
+        {
+          label: 'boot_on_random',
+          name: '起動時にランダム表示',
+          value: false,
+        },
+        {
+          label: 'notify_ip_changed',
+          name: 'IP変更後の起動時に通知する',
+          value: false,
+        },
+      ]
+    }
+  },
+
+  methods: {
+    onChangeValue() {
+    },
+  },
+}
+</script>
+
+<style>
+
+.settings {
+  margin: 16px;
+}
+
+</style>
