d769594ce24d90ff66d70e5c4402c24fe45642ce
naoki <naokipgt@gmail.com>
2021-11-14 15:30:48 +0900

    issue-37 起動時にコンフィグを読み込むようにした， コンフィグ変更をサーバに反映するようにした

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -5,34 +5,48 @@
         v-model="param.value"
         :label="param.name"
         color="primary"
-        @change="onChangeValue"
+        @change="onChangeValue(param)"
       />
     </div>
   </div>
 </template>
 
 <script>
+const axios = require('axios')
 export default {
 
   data() {
     return {
-      params: [
-        {
+      params: {
+        boot_on_random: {
           label: 'boot_on_random',
           name: '起動時にランダム表示',
           value: false,
         },
-        {
+        notify_ip_changed: {
           label: 'notify_ip_changed',
           name: 'IP変更後の起動時に通知する',
           value: false,
         },
-      ]
+      }
+    }
+  },
+
+  created: async function() {
+    const result = await axios.get('/api/config')
+    const configList = result.data
+    for(const param in configList) {
+      this.params[param].value = configList[param]
     }
   },
 
   methods: {
-    onChangeValue() {
+    onChangeValue: async function(param) {
+      await axios.post('/api/config', {
+        label: param.label,
+        value: param.value,
+        }
+      )
     },
   },
 }
